7376f36259e030cd0ff22b44d4a553aea490132e
Rachel Schieferstein <violenza@gmail.com>
2020-01-17 11:25:22 -0800

    added jquery code to retain form input. tested with console.log succesfully

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $("form#questions").submit(function(event) {
+    var age = parseInt($("input#age").val());
+    console.log(age)
+    
+    var ears = $("#ears").val();
+    console.log(ears)
+    
+    var war = $("input:radio[name=war]:checked").val();
+    console.log(war)
+    
+    var pokemon = $("input:radio[name=pokemon]:checked").val();
+    console.log(pokemon)
+
+    var god = $("#god").val();
+    console.log(god)
+
+    var drink = $("#drink").val();
+    console.log(drink)
+
+    var quote = (100 - age) * 3;
+
+    $("#rate").empty().append(quote);
+    $("#quote").show();
+
+    event.preventDefault();
+  });
+});
